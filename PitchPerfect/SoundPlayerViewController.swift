d657e6a7b345f9fba2ec14a1cda233708b847340
Nicolas Jasmes <jasmesn@Nicolass-MacBook-Pro.local>
2015-08-13 18:43:53 +0200

    Add Application Icon - 120 for 2x & 180 for 3x

--- PitchPerfect/SoundPlayerViewController.swift
+++ PitchPerfect/SoundPlayerViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SoundPlayerViewController.swift
 //  PitchPerfect
 //
 //  Created by Nicolas Jasmes on 13/08/15.
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SoundPlayerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
