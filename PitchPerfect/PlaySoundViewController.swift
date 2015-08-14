4275bddf24dea60327693954c337c313a548dd4a
Nicolas Jasmes <jasmesn@Nicolass-MacBook-Pro.local>
2015-08-14 08:20:14 +0200

    Design of the main "recording" view controller. (Place objects + constraints)

--- PitchPerfect/PlaySoundViewController.swift
+++ PitchPerfect/PlaySoundViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SoundPlayerViewController: UIViewController {
+class PlaySoundViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- PitchPerfect/RecordSoundViewController.swift
+++ PitchPerfect/RecordSoundViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class RecordSoundViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
